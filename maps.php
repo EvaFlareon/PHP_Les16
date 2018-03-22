3c54127732838deba7380355a08c9c56d341fcb1
Евлахина Анна <35760192+EvaFlareon@users.noreply.github.com>
2018-03-22 20:28:15 +0300

    Add files via upload

---
+++ maps.php
@@ -0,0 +1,40 @@
+<?php
+
+if (isset($_GET['Latitude']) && isset($_GET['Longitude'])) {
+	$Latitude = $_GET['Latitude'];
+	$Longitude = $_GET['Longitude'];
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="utf-8">
+	<title>Карта</title>
+	<script src="https://api-maps.yandex.ru/2.1/?lang=ru_RU" type="text/javascript"></script>
+    <script type="text/javascript">
+        ymaps.ready(init);
+        var myMap, 
+            myPlacemark;
+
+        function init(){ 
+            myMap = new ymaps.Map("map", {
+                center: [<?= $Latitude ?>, <?= $Longitude ?>],
+                zoom: 7
+            }); 
+            
+            myPlacemark = new ymaps.Placemark([<?= $Latitude ?>, <?= $Longitude ?>], {
+                hintContent: 'Выбранное Вами место',
+                balloonContent: 'Вам точно туда надо?'
+            });
+            
+            myMap.geoObjects.add(myPlacemark);
+        }
+    </script>
+</head>
+<body>
+	<div id="map" style="width: 600px; height: 400px"></div>
+	<a href="index.php">Вернуться к поиску</a>
+</body>
+</html>
